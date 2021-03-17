d0b485321458733d1334008cc3b9427c10f25258
nicolasvidelac <nicolasvidelac@gmail.com>
2021-03-17 16:48:06 -0300

    bunch of changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 import schedule
 import requests
 import sqlite3
@@ -12,8 +12,10 @@ connection = sqlite3.connect('flexpool.db')
 cursor = connection.cursor();
 init(convert=True)
 
-apiKey = str(os.environ.get('ETHERSCAN_APIKEY'))
+apiKeyEther = str(os.environ.get('ETHERSCAN_APIKEY'))
+apiKeyGasStation = str(os.environ.get('ETHGASSTATION_APIKEY'))
 wallet = str(os.environ.get('ETH_WALLET'))
+
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS PROFIT
     (
@@ -31,8 +33,9 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS BALANCE
 
 def profitCalculator(save = True):
     url = 'https://flexpool.io/api/v1/miner/%s/balance'%wallet
-    response = requests.get(url)
 
+    response = requests.get(url)
+    
     todaysBalance = gweiToEth(response.json()['result'])
 
     cursor.execute("SELECT * FROM BALANCE")
@@ -52,9 +55,20 @@ def profitCalculator(save = True):
 
     ethPrice = getEthPrice()
 
+    minutosActual = datetime.now().minute + datetime.now().hour * 60 #la cantidad de minutos de hoy
+
+    minedMinutes = 0;
+    if minutosActual >= 9 * 60:
+        minedMinutes = minutosActual - 9 * 60
+    else:
+        minedMinutes = minutosActual + 15 * 60
+
+    expectedTodaysEarnings = todaysProfit / minedMinutes * (24 * 60)
+
     print(
-        "ganancias de hoy: ", str(truncate(todaysProfit,6)), "eth / $", "{0:0>5}".format(truncate(todaysProfit * ethPrice,2)), "usd",
-        "\nganancias totales:", str(truncate(todaysBalance,6)), "eth / $", truncate(todaysBalance * ethPrice,2), "usd",
+        "ganancias hasta ahora: ", str(truncate(todaysProfit,6)), "eth / $", "{0:0>5}".format(truncate(todaysProfit * ethPrice,2)), "usd",
+        "\nganancias esperadas:   ", str(truncate(expectedTodaysEarnings, 6)), "eth / $", "{:0>5}".format(truncate(expectedTodaysEarnings * ethPrice,2)), "usd",
+        "\n\nganancias totales:", str(truncate(todaysBalance,6)), "eth / $", truncate(todaysBalance * ethPrice,2), "usd",
         "\nprecio de eth:   $", ethPrice, "usd"
     )
 
@@ -72,16 +86,17 @@ def expectedEarnings():
     return;
 
 def getGastPrice():
-    url = 'https://www.ethgasstation.info/https://ethgasstation.info/api/ethgasAPI.json?api-key=4a2aed973779b603681e6970b3a2091ad721e7c25fd9f4d59f0eeddc5a99'
+    url = 'https://www.ethgasstation.info/https://ethgasstation.info/api/ethgasAPI.json?api-key=%s'%apiKeyGasStation
     response = (requests.get(url)).json()
 
     gasPrice = float(response['safeLow'] / 10)
     transactionFee = 21000
     usdPrice = getEthPrice()
 
-    print(int(gasPrice), "gwei")
-    print(str(gasPrice / 1000000000 * transactionFee * usdPrice)[:4], "usd")
-
+    print(
+        int(gasPrice), "gwei\n" +
+        str(gasPrice / 1000000000 * transactionFee * usdPrice)[:4], "usd"
+    )
 
 def dailyReport():
     url = 'https://flexpool.io/api/v1/miner/%s/daily'%wallet
@@ -102,6 +117,7 @@ def dailyReport():
 
 def getHistory():
     cursor.execute("SELECT * FROM PROFIT")
+
     profit = cursor.fetchall()[-7:]
     #Que solo me muestre el historial de la ultima semana
 
@@ -110,13 +126,18 @@ def getHistory():
 
     ethPrice = getEthPrice()
 
-    print("ganancias totales:", truncate(balance,6), "eth\n")
-    
+    total = 0
+
     for item in reversed(profit):
+        total += item[1]
         print("generado:", format(truncate(item[1],4),'.4f'),"eth / $", format(truncate(item[1] * ethPrice,2),'.2f') , "// fecha:", item[0][5:10])
-        
+    
+    print("\nganancias ultima semana:", truncate(total,4), "eth / $", format(truncate(total * ethPrice,2),'.2f'))
+    promedio = truncate(total / 7,4)
+    print("promedio  ultima semana:", promedio, "eth / $", format(truncate(promedio * ethPrice,2),'.2f'))
+
 def getEthPrice():
-    url = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey=%s'%apiKey
+    url = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey=%s'%apiKeyEther
     response = requests.get(url).json()['result']
     return float(response['ethusd']) 
 
@@ -142,25 +163,24 @@ while True:
         expectedEarnings()
 
     elif keyboard.is_pressed('3'):
-        print(f"\n{Fore.LIGHTCYAN_EX}Profit history at", str(datetime.now().time())[:5] + ":")
+        print(f"\n{Fore.LIGHTCYAN_EX}Profit history at", str(datetime.now().time())[:5] + ":\n")
         getHistory();
 
     elif keyboard.is_pressed('4'):
-        print(f"\n{Fore.LIGHTMAGENTA_EX}Gas price at", str(datetime.now().time())[:5] + ":")
+        print(f"\n{Fore.MAGENTA}Gas price at", str(datetime.now().time())[:5] + ":")
         getGastPrice()
 
     elif keyboard.is_pressed('5'):
         print(f"\n{Fore.LIGHTWHITE_EX}Daily report at", str(datetime.now().time())[:5] + ":")
         dailyReport()
 
-    elif keyboard.is_pressed('6'):
+    elif keyboard.is_pressed('h'):
         print(
             f"\n{Fore.LIGHTGREEN_EX}Press 1 for current profits",
             f"\n{Fore.LIGHTYELLOW_EX}Press 2 for expected earnings",
             f"\n{Fore.LIGHTCYAN_EX}Press 3 for profits history",
             f"\n{Fore.LIGHTMAGENTA_EX}Press 4 for gas price",
             f"\n{Fore.LIGHTWHITE_EX}Press 5 for daily profits",
-
         )
 
     schedule.run_pending()
